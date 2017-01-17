fd38bb0860cd8d7c9ddb86ecf8aa746c1ea1523f
deandt100 <dean100@live.co.za>
2017-01-17 08:33:00 +0200

    first commit - testin git

---
+++ Dean/hello_world/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// Dean du Toit BBD cpp Bootcamp
+
+#include "stdafx.h" //Windows needs this
+#include <iostream>
+
+int main()
+{
+	using std::cout; // Just using namespace std is unecissary
+	using std::endl;
+	using std::cin;
+
+	cout << "Hello World" << endl;
+	cin.get();
+    return (0);
+}
+
